607d6517233f40bde4daa78d38d7c108826b0ffa
403-12-Patapee <54429@yupparaj.ac.th>
2026-08-24 13:21:39 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,4 +101,4 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2, ans3, ans4)
 st.divider()
-    st.write("นายปฐพี วิละคำ เลขที่ 12 ม.4/3")
+st.write("นายปฐพี วิละคำ เลขที่ 12 ม.4/3")
