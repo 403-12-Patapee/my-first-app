e6b6cef13e986e32ba81df7445b99fe3ca62001b
403-12-Patapee <54429@yupparaj.ac.th>
2026-08-24 13:25:02 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -26,31 +26,31 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     st.balloons()
     score = 0
 
-u_ans1 = ans1.strip().lower()
-u_ans2 = ans2.strip().lower()
-u_ans3 = ans3.strip().lower()
-u_ans4 = ans4.strip().lower()
+    u_ans1 = ans1.strip().lower()
+    u_ans2 = ans2.strip().lower()
+    u_ans3 = ans3.strip().lower()
+    u_ans4 = ans4.strip().lower()
 
 
-if u_ans1 == "apple":
+    if u_ans1 == "apple":
     st.success("✅ ข้อ 1: ถูกต้อง")
     score += 1
-else:
+    else:
     st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
-if u_ans2 == "fish":
+    if u_ans2 == "fish":
     st.success("✅ ข้อ 2: ถูกต้อง")
     score += 1
-else:
+    else:
     st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
-if u_ans3 == "wings":
+    if u_ans3 == "wings":
     st.success("✅ ข้อ 3: ถูกต้อง")
     score += 1
-else:
+    else:
     st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
-if u_ans4 == "imposter":
+    if u_ans4 == "imposter":
     st.success("✅ ข้อ 4: ถูกต้อง")
     score += 1
-else:
+    else:
     st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
 
 
