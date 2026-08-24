ee2ee0098533b47051b6822e31fb361cc6472dc0
403-12-Patapee <54429@yupparaj.ac.th>
2026-08-24 13:15:29 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -57,7 +57,7 @@ else:
 st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 if score == 4:
     st.success("🎉 You win! Hooray!!")
-   else:
+ else:
     st.error("You lose!")
 
 st.button("🎮 เริ่มเล่นเกม", on_click=reset_game)
